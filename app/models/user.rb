cab82255142a5c33a43778756c8e2c9112ad9733
Leandro <leandromarte@gmail.com>
2017-04-12 14:03:58 +0000

    Bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          
   has_one :payment
-  accepst_nested_attributes_for :payment
+  accepts_nested_attributes_for :payment
 end
